2902bff29cf688c1df444ff6bb4d791ab49471bf
wanghuachen <513239407@qq.com>
2023-08-09 09:55:01 +0800

    rust
    Signed-off-by: wanghuachen <513239407@qq.com>

--- src/sched/execute_ctx.h
+++ src/sched/execute_ctx.h
@@ -99,7 +99,7 @@ struct ExecuteCtx {
             return false;
         }
         if (*priority_task_ptr == nullptr) {
-            *priority_task_ptr == task;
+            *priority_task_ptr = task;
             return true;
         }
         return false;

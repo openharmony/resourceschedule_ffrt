d1bb64cb0d6ce0dba389e455f10412cfaed5e3b5
openharmony_ci <120357966@qq.com>
2024-02-02 06:44:43 +0000

    回退 'Pull Request !136 : poller构造顺序修复 & 使能长耗时任务监测'

--- src/util/worker_monitor.cpp
+++ src/util/worker_monitor.cpp
@@ -29,6 +29,9 @@ constexpr uint64_t PROCESS_NAME_BUFFER_LENGTH = 1024;
 constexpr uint64_t MONITOR_TIMEOUT_MAX_COUNT = 2;
 constexpr uint64_t MONITOR_SAMPLING_CYCLE_US = 500 * 1000;
 constexpr uint64_t TIMEOUT_RECORD_CYCLE_US = 60 * 1000 * 1000;
+
+using UvFunc = void(*)(void*);
+constexpr unsigned int UV_FUNC_OFFSET = 2 *sizeof(uintptr_t);
 }
 
 namespace ffrt {
@@ -39,7 +42,6 @@ WorkerMonitor::WorkerMonitor()
     // hdc在调用hdc shell的时候会长期占用worker，过滤该进程以防止一直打印超时信息
     // 另外，对hdc进程进行监控会概率性导致hdc断连，原因未知，暂时规避
     skipSampling_ = (strstr(processName, "hdcd") != nullptr);
-    SubmitSamplingTask();
 }
 
 WorkerMonitor::~WorkerMonitor()
@@ -108,24 +110,23 @@ void WorkerMonitor::RecordTimeoutFunctionInfo(WorkerThread* worker, CPUEUTask* w
 
 void WorkerMonitor::RecordSymbolAndBacktrace(CPUEUTask* task, int tid)
 {
-    FFRT_LOGW("Function occupies worker for more than 1s.");
-
-#ifdef FFRT_OH_TRACE_ENABLE
-    std::string dumpInfo;
-    if (OHOS::HiviewDFX::GetBacktraceStringByTid(dumpInfo, tid, 0, false)) {
-        FFRT_LOGW("Backtrace:\n%s", dumpInfo.c_str());
-    }
-#else
     void* func = nullptr;
     if (task->type != 0) {
-        func = reinterpret_cast<void*>(task);
+        UvFunc* funcAddr = reinterpret_cast<UvFunc*>(reinterpret_cast<char*>(task) - UV_FUNC_OFFSET);
+        func = reinterpret_cast<void*>(*funcAddr);
     }
 
     Dl_info info;
     if (dladdr(func, &info)) {
-        FFRT_LOGW("Function [%s] in [%s].", (info.dli_sname ? info.dli_sname : "unknown"),
-            (info.dli_fname ? info.dli_fname : "unknown"));
-    }
+        FFRT_LOGW("Function [%s] in [%s] occupies worker for more than 1s.",
+            (info.dli_sname ? info.dli_sname : "unknown"), (info.dli_fname ? info.dli_fname : "unknown"));
+
+#ifdef FFRT_OF_TRACE_ENABLE
+        std::string dumpInfo;
+        if (OHOS::HiviewDFX::GetBacktraceStringByTid(dumpInfo, tid, 0, false)) {
+            FFRT_LOGW("Backtrace:\n%s", dumpInfo.c_str());
+        }
 #endif
+    }
 }
 }

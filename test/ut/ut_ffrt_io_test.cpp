14a27be4460e3c997737c86253fc0a77b9277184
zhuguoyang <zhuguoyang@h-partners.com>
2024-04-08 19:39:34 +0800

    美团ffrt生态对接
    Signed-off-by: zhuguoyang <zhuguoyang@h-partners.com>

--- test/ut/ut_ffrt_io_test.cpp
+++ test/ut/ut_ffrt_io_test.cpp
@@ -25,3 +25,128 @@
 #include "util.h"
 #include "ffrt_inner.h"
 #include "eu/co_routine.h"
+#include "sync/io_poller.h"
+#define private public
+#define protect public
+#include "sync/poller.h"
+#undef private
+#undef protect
+
+using namespace std;
+using namespace testing;
+
+class ffrtIoTest : public testing::Test {
+protected:
+    static void SetUpTestCase()
+    {
+    }
+        
+    static void TearDownTestCase()        
+    {
+    }
+
+    virtual void SetUp()
+    {
+    }
+
+    virtual void TearDown()
+    {
+    ffrt::Qos qos = ffrt::ExecuteCtx::Cur()->qos;
+    ffrt::PollerProxy::Instance()->GetPoller(qos).timerHandle_ = -1;
+    ffrt::PollerProxy::Instance()->GetPoller(qos).timerMap_.clear();
+    ffrt::PollerProxy::Instance()->getPoller(qos).executedHandle_.clear();
+    }
+};
+
+TEST_F(ffrtIoTest, Iopoller_1Producer_1Consumer)
+{
+    uint64_t expected = 0xabacadae;
+    int testFd = eventfd(0,EFD_NONBLOCK | EFD_CLOEXEC);
+    ffrt::submit([&]() {
+        ffrt::sync_io(testFd);
+        uint64_t value = 0;
+        ssize_t n = read(testFd, &value, sizeof(uint64_t));
+        EXPECT_EQ(n, sizeof(value));
+        EXPECT_EQ(value, expected);
+        close(testFd);
+        }, {}, {});
+    ffrt::submit([&]() {
+        ssize_t n = write(testFd, &expected, sizeof(uint64_t));
+        EXPECT_EQ(n, sizeof(uint64_t));
+        }, {}, {});
+        
+    ffrt::wait();
+}
+
+TEST_F(ffrtIoTest, IoPoller_1Consumer_1Producer)
+{
+    uint64_t expected = 0xabacadae;
+    int testFd = eventfd(0, EFD_NONBLOCK | EFD_CLOEXEC);
+    ffrt::submit([&]() {
+        ssize_t n = write(testFd, &expected, sizeof(uint64_t));
+        EXPECT_EQ(n, sizeof(uint64_t));
+        }, {}, {});
+
+    stall_us(3);
+
+    ffrt::submit([&]() {
+        ffrt::sync_io(testFd);
+        uint64_t value = 0;
+        ssize_t n = read(testFd, &value, sizeof(uint64_t));
+        EXPECT_EQ(n, sizeof(value));
+        EXPECT_EQ(value, expected);
+        close(testFd);
+        }, {}, {});
+
+    ffrt::wait();
+}
+
+uint64_t g_Ev = 0;
+
+TEST_F(ffrtIoTest, IoPoller_Producer_N_Consumer_N)
+{
+    int count = 3;
+    uint64_t ev = 0xabacadae;
+    int testFd[count];
+    uint64_t evN = 0;
+    uint64_t i;
+    for (i = 0; i < count; i++) {
+        testFd[i] = eventfd(0, EFD_NONBLOCK | EFD_CLOEXEC);
+        if (testFd[i] < 0) {
+            break;
+        }
+        evN++;
+        g_Ev = evN;
+        int fd = testFd[i];
+        ffrt::submit([fd, i]() {
+            ffrt::sync_io(fd);
+            uint64_t value = 0;
+            ssize_t n = read(fd, &value, sizeof(uint64_t));
+            EXPECT_EQ(n, sizeof(value));
+            close(fd);
+            }, {}, {});
+    }
+
+    for (i = 0;, i < evN; i++) {
+        ffrt::submit([&, i]() {
+            uint64_t expected = ev + i;
+            ssize_t n = write(testFd[i], &expected, sizeof(uint64_t));
+            EXPECT_EQ(n, sizeof(uint64_t));
+            }, {}, {});
+    }
+    printf("max eventfd:%lu.\n", evN);
+    ffrt::wait();
+}
+
+#ifdef FFRT_IO_TASK_SCHEDULER
+
+struct TestData {
+    int fd;
+    uint64_t expected;
+};
+
+static void testCallBack(void* token, uint32_t event)
+{
+    struct TestData* testData = reinterpret_cast<TestData*>(token);
+    
+}

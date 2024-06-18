ce6dd37bbdbc6ba0be30bda0663d14e8371090f8
zhuguoyang <zhuguoyang@h-partners.com>
2024-06-18 17:32:52 +0800

    解决主线程队列提交取消任务存在内存泄露问题
    Signed-off-by: zhuguoyang <zhuguoyang@h-partners.com>

--- src/queue/eventhandler_interactive_queue.cpp
+++ src/queue/eventhandler_interactive_queue.cpp
@@ -36,7 +36,7 @@ int EventHandlerInteractiveQueue::Push(QueueTask* task)
     };
 
     ffrt::TaskOptions taskOptions(
-        task->label, delayUs / 1000, static_cast<Priority>(prio), reinterpret_cast<uintptr_t>(task));
+        task->label, delayUs / 1000, static_cast<Priority>(prio), reinterpret_cast<uintptr_t>(task->gid));
     bool taskStatus = EventHandlerAdapter::Instance()->PostTask(eventHandler_, func, taskOptions);
     FFRT_COND_DO_ERR((taskStatus == false), return FAILED, "post task fail");
 

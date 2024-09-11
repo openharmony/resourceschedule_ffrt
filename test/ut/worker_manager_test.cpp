73a9bf092d6239523ebd2c8c701a3c83979d69b4
fanyaohua <fanyaohua@h-partners.com>
2024-09-11 18:27:46 +0800

    sync ut code
    Signed-off-by: fanyaohua <fanyaohua@h-partners.com>

--- test/ut/worker_manager_test.cpp
+++ test/ut/worker_manager_test.cpp
@@ -24,10 +24,11 @@
 #include "sched/workgroup_internal.h"
 #include "common.h"
 
-using namespace ffrt;
+using namespace testing;
 #ifdef HWTEST_TESTING_EXT_ENABLE
 using namespace testing::ext;
 #endif
+using namespace ffrt;
 
 class WorkerManagerTest : public testing::Test {
 protected:

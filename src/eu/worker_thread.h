1c9c69661faf0816357f4312d028100cfc2345dc
yuanzhipu <2710242656@qq.com>
2024-11-23 16:35:11 +0800

    set prio for camera in ohos
    Signed-off-by: yuanzhipu <2710242656@qq.com>

--- src/eu/worker_thread.h
+++ src/eu/worker_thread.h
@@ -209,7 +209,11 @@ private:
     unsigned int domain_id;
 #endif
 };
+constexpr int CFS_PRIO_MIN = 1;
+constexpr int CFS_PRIO_DEFAULT = 20;
+constexpr int CFS_PRIO_MAX = 40;
+constexpr int VIP_PRIO_MIN = 41;
+constexpr int VIP_PRIO_MAX = 50;
 void SetThreadAttr(WorkerThread* thread, const QoS& qos);
-void SetThreadPriority(unsigned char priority, WorkerThread* thread);
 } // namespace ffrt
 #endif

99f4f30eed61b35c1ae627e3769f323091fd0ec6
董洁 <dongjie52@h-partners.com>
2024-11-26 14:10:36 +0800

    eventhandler queue task optimize iteration logic
    Signed-off-by: 董洁 <dongjie52@h-partners.com>

--- src/queue/base_queue.h
+++ src/queue/base_queue.h
@@ -73,7 +73,7 @@ public:
         return queueId_;
     }
 
-    bool HasTask(const char* name);
+    virtual bool HasTask(const char* name);
 
     bool HasLock()
     {
@@ -94,7 +94,11 @@ protected:
             std::chrono::steady_clock::now().time_since_epoch()).count();
     }
 
-    void ClearWhenMap();
+    void Stop(std::multimap<uint64_t, QueueTask*>& whenMap);
+    void Remove(std::multimap<uint64_t, QueueTask*>& whenMap);
+    int Remove(const QueueTask* task, std::multimap<uint64_t, QueueTask*>& whenMap);
+    int Remove(const char* name, std::multimap<uint64_t, QueueTask*>& whenMap);
+    bool HasTask(const char* name, std::multimap<uint64_t, QueueTask*> whenMap);
 
     const uint32_t queueId_;
     bool isExit_ { false };

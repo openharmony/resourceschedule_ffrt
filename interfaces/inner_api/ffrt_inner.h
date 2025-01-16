01014383852e25196338f8c8d620b135b9064e07
wenxingqi <wenxingqi1@h-partners.com>
2025-01-16 09:42:16 +0800

    5.1 ffrt self-loop
    Signed-off-by: wenxingqi <wenxingqi1@h-partners.com>

--- interfaces/inner_api/ffrt_inner.h
+++ interfaces/inner_api/ffrt_inner.h
@@ -32,6 +32,9 @@
 #include "c/executor_task.h"
 #include "c/shared_mutex.h"
 #include "c/ffrt_dump.h"
+#include "c/deadline.h"
+#include "c/ffrt_cpu_boost.h"
+#include "c/ffrt_ipc.h"
 #include "c/init.h"
 #endif
 #endif

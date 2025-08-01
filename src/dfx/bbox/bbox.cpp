42996327b1faa2c0333874beda460e946db0a5d7
Lilith Oberhauser <lilith.oberhauser@huawei.com>
2025-08-01 17:20:22 +0300

    Fix data race on `primaryCache`. A thread calls `SaveNormalTaskStatus`->`GetUnfreedMem` without lock protection. Another thread writes to the `primaryCache`. Fix the data race by using lock protection.
    Issue: https://gitee.com/openharmony/resourceschedule_ffrt/issues/ICQIII
    
    Signed-off-by: Lilith Oberhauser <lilith.oberhauser@huawei.com>

--- src/dfx/bbox/bbox.cpp
+++ src/dfx/bbox/bbox.cpp
@@ -164,7 +164,9 @@ static inline void SaveKeyStatus()
 
 static inline void SaveNormalTaskStatus()
 {
+    TaskFactory<CPUEUTask>::LockMem();
     auto unfree = TaskFactory<CPUEUTask>::GetUnfreedMem();
+    TaskFactory<CPUEUTask>::UnlockMem();
     auto apply = [&](const char* tag, const std::function<bool(CPUEUTask*)>& filter) {
         std::vector<CPUEUTask*> tmp;
         for (auto task : unfree) {
@@ -174,7 +176,6 @@ static inline void SaveNormalTaskStatus()
                 tmp.emplace_back(t);
             }
         }
-
         if (tmp.size() > 0) {
             FFRT_BBOX_LOG("<<<=== %s ===>>>", tag);
         }
@@ -199,7 +200,6 @@ static inline void SaveNormalTaskStatus()
             }
         }
     };
-
     // Do not dump tasks marked with a final status (e.g., FINISH or CANCELED),
     // as they may be allocated by another submit and not initialized yet.
     apply("pending task", [](CPUEUTask* t) {
@@ -255,7 +255,9 @@ static void DumpQueueTask(const char* tag, const std::vector<QueueTask*>& tasks,
 
 static inline void SaveQueueTaskStatus()
 {
-    auto unfreeQueueTask = SimpleAllocator<QueueTask>::getUnfreedMem();
+    TaskFactory<QueueTask>::LockMem();
+    auto unfreeQueueTask = TaskFactory<QueueTask>::GetUnfreedMem();
+    TaskFactory<QueueTask>::UnlockMem();
     if (unfreeQueueTask.size() == 0) {
         return;
     }

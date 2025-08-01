ad9bc70f2d1860a484378a8f939d877235b3b6ea
Lilith Oberhauser <lilith.oberhauser@huawei.com>
2025-08-01 16:19:49 +0300

    Fix data-race on curTask. A thread calling SaveWorkerStatus reads curTask, while another thread writes to curTask in RunTask. Make curTask an atomic field. Read and write atomically with relaxed memory order.
    Issue: https://gitee.com/openharmony/resourceschedule_ffrt/issues/ICQIE4
    Signed-off-by: Lilith Oberhauser <lilith.oberhauser@huawei.com>

--- include/eu/cpu_worker.h
+++ include/eu/cpu_worker.h
@@ -186,7 +186,7 @@ public:
 #endif
 
     void SetThreadAttr(const QoS& newQos);
-    TaskBase* curTask = nullptr;
+    std::atomic<TaskBase*> curTask = nullptr;
     std::atomic<uintptr_t> curTaskType_ {ffrt_invalid_task};
     std::string curTaskLabel_ = ""; // 需要打开宏WORKER_CAHCE_NAMEID才会赋值
     uint64_t curTaskGid_ = UINT64_MAX;

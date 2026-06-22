6e39abea6ceb62c57381d5a826bcb3b6cedd8f40
huanghf <huanghaifei3@h-partners.com>
2026-06-22 16:18:20 +0800

    debt iteration second
    Signed-off-by: huanghf <huanghaifei3@h-partners.com>

--- src/eu/sexecute_unit.cpp
+++ src/eu/sexecute_unit.cpp
@@ -45,6 +45,7 @@ const int SEXECUTE_DESTRY_SLEEP_TIME = 1000;
 const std::map<std::string,
     void(*)(ffrt::SExecuteUnit*, const ffrt::QoS&, ffrt::TaskNotifyType)> NOTIFY_FUNCTION_FACTORY = {
     { "CameraDaemon", ffrt::SExecuteUnit::HandleTaskNotifyConservative },
+    { "bluetooth_service", ffrt::SExecuteUnit::HandleTaskNotifyUltraConservative },
 };
 }
 
